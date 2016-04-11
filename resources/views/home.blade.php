9d2fae6fa91b96aa4d45ba5a4644ec8c692895b5
webteach <development@teachknowledgy.com>
2016-04-11 01:20:48 +0000

    Changes to Views ONLY

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+<div>
+    <h1>This is the dashboard page!</h1>
+    <a href="{{ url('/account') }}">Manage My Account</a>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
-        </div>
-    </div>
+    <h1>This is the landing page for the app!</h1>
+    <p>Welcome to Teachknowledgy.  This is a web application that works for classroom teachers.  Manage your data effectively and efficiently so that you data does NOT manage you.</p>
 </div>
 @endsection
